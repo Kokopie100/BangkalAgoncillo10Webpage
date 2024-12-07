61a324ea24bd5a2bf72ee5fdd49a46da61156e7a
Kianail <kianandrei.alvarado@depedmakati.ph>
2024-12-07 18:23:39 +0800

    Update Agon.js

--- Agon.js
+++ Agon.js
@@ -25,3 +25,16 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
