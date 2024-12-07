489810ea15020630a3a013e0eccf1559dfe604e9
Kianail <kianandrei.alvarado@depedmakati.ph>
2024-12-07 14:46:59 +0800

    Update Agon.js

--- Agon.js
+++ Agon.js
@@ -1,27 +1,12 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+var images = document.getElementById("image-container").children;
+let i = images.length-1;
+var interval = setInterval(function(){
+  //increase opacity of current image
+  if(images[i].style.opacity){
+    images[i].style.opacity-=1/50;
+  } else images[i].style.opacity = 1.0;
+  //if current image invisble then works with next
+  if(images[i].style.opacity<=0)i--;
+  //if last then stop interval
+  if(i==-1)clearInterval(interval)
+},50);
